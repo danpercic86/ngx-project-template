7ebc8c35e5ec24e3542732871f533e69a589a817
Dan Percic <danpercic86@gmail.com>
2022-06-19 16:35:48 +0300

    add husky & lint staged

--- src/ngx-project-template/index.ts
+++ src/ngx-project-template/index.ts
@@ -1,22 +1,18 @@
-import {externalSchematic, Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
-
+import { externalSchematic, Rule } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function ngxProjectTemplate(_options: any): Rule {
-    const name = _options.name
-    console.log('The name of the repo will be', name)
+    const name = _options.name;
+    console.log('The name of the repo will be', name);
 
-    return (_: Tree, _context: SchematicContext) => {
-        return externalSchematic('@schematics/angular', 'ng-new', {
+    return () =>
+        externalSchematic('@schematics/angular', 'ng-new', {
             name,
-            version: '9.0.0',
             directory: name,
-            routing: false,
+            version: '14.0.0',
             style: 'scss',
-            inlineStyle: false,
-            inlineTemplate: false,
             createApplication: false,
+            packageManager: 'yarn',
         });
-    };
 }
